2dee71766c5c5ef701d6c1f56adb2e0d8cf7e5de
Vedant Kumar <vedantvisoliya@gmail.com>
2025-08-24 13:48:40 +0530

    added components and utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Header from "./components/header"
+import SideNav from "./components/sideNav"
+import PokeCard from "./components/pokeCard"
 
 function App() {
 
   return (
     <>
+      <Header />
+      <SideNav />
+      <PokeCard />
     </>
   )
 }
